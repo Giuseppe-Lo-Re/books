fc0885a576fb97666137181ac2752d35d38631fb
Giuseppe Lo Re <giuseppe.lore@dev4side.com>
2023-03-09 16:44:16 +0100

    Updated title

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ import BookList from "./components/BookList";
 function App() {
     const [books, setBooks] = useState([]); 
 
+    const editBookById = (id, newTitle) => {
+        const updatedBooks = books.map((book) => {
+            if(book.id === id) {
+                return {...book, title: newTitle};
+            }
+
+            return book;
+        });
+
+        setBooks(updatedBooks);
+    };
+
     const deleteBookById = (id) => {
         const updatedBooks = books.filter((book) => {
             return book.id !== id;
@@ -22,7 +34,11 @@ function App() {
 
     return(
         <div className="app">
-            <BookList onDelete={deleteBookById} books={books} />
+            <BookList 
+                onEdit={editBookById} 
+                books={books} 
+                onDelete={deleteBookById} 
+            />
             <BookCreate onCreate={createBook}/>
         </div>
     )
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,14 @@
 import BookShow from "./BookShow";
 
-function BookList({ books, onDelete }) {
+function BookList({ books, onDelete, onEdit }) {
     const renderedBooks = books.map((book) => {
-        return <BookShow onDelete={onDelete} key={book.id} book={book} />;
-    })
+        return <BookShow 
+                    onEdit={onEdit} 
+                    onDelete={onDelete} 
+                    key={book.id} 
+                    book={book} 
+                />;
+    });
 
     return (
         <div className="book-list">
